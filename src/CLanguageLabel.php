42933bff4c55df6c327e4fbf3b1eab5024775f0c
peterrstanley <peterrstanley@gmail.com>
2013-01-25 00:21:45 -0500

    - Attempted to remove CLabelLanguage.php from repo (replaced by CLanguageLabel.php) - Removed PDO clutches as mentioned in Issue #2. - Unified bracket indentation on the code as mentioned in Issue #3 - Had class contruct receive PDO object instead of creating a new internal PDO object as mentioned in Issue #1

--- src/CLanguageLabel.php
+++ src/CLanguageLabel.php
@@ -1,14 +1,10 @@
 <?php
 
-class CLanguageLabel
-{
+class CLanguageLabel {
 	var $def_lang = 'en';
 	
-	public function __construct($_dbhost,$_dbname,$_dbuser,$_dbpass){
-		$this->dsn = 'mysql:host='.$_dbhost.';dbname='.$_dbname;
-		$this->user = $_dbuser;
-		$this->pass = $_dbpass;	
-		$this->pdo = new PDO($this->dsn,$this->user,$this->pass);
+	public function __construct($_pdo){
+		$this->pdo = $_pdo;
 		
 		if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
 			$lang_list = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
@@ -24,8 +20,7 @@ class CLanguageLabel
 	}
 	
 	public function add_label($label){
-		$pdo = $this->pdo;
-		$check = $pdo->prepare("SELECT * FROM lang_label WHERE label = ?");
+		$check = $this->pdo->prepare("SELECT * FROM lang_label WHERE label = ?");
 		$check->execute(array($label));
 		if($check->rowCount() < 1){
 			$ins = $pdo->prepare("INSERT INTO lang_label (label) VALUES(?)");
@@ -35,33 +30,30 @@ class CLanguageLabel
 	}
 	
 	public function delete_label($label){
-		$pdo = $this->pdo;
-		$select = $pdo->prepare("SELECT id FROM lang_label WHERE label = ?");
+		$select = $this->pdo->prepare("SELECT id FROM lang_label WHERE label = ?");
 		$select->execute(array($label));
 		$res1 = $select->fetchAll(PDO::FETCH_ASSOC);
 		
-		$del1 = $pdo->prepare("DELETE FROM lang_label WHERE id = ?");
-		$del2 = $pdo->prepare("DELETE FROM lang_label_data WHERE labelid = ?");
+		$del1 = $this->pdo->prepare("DELETE FROM lang_label WHERE id = ?");
+		$del2 = $this->pdo->prepare("DELETE FROM lang_label_data WHERE labelid = ?");
 		$del1->execute(array($res1[0]['id']));
 		$del2->execute(array($res1[0]['id']));
 	}
 	
-	public function get_label_value($label,$def = ''){
-		$pdo = $this->pdo;
-		$langid = ($def == '') ? $this->def_lang : $def;
-		$lth = $pdo->prepare("SELECT id FROM lang_label WHERE label = ?");
-		$vth = $pdo->prepare("SELECT value FROM lang_label_data WHERE labelid = ? AND langid = ?");
+	public function get_label_value($label,$lang = ''){
+		$langid = ($lang == '') ? $this->def_lang : $lang;
+		$lth = $this->pdo->prepare("SELECT id FROM lang_label WHERE label = ?");
+		$vth = $this->pdo->prepare("SELECT value FROM lang_label_data WHERE labelid = ? AND langid = ?");
 		$lth->execute(array($label));
 		$res1 = $lth->fetchAll(PDO::FETCH_ASSOC);
 		$vth->execute(array($res1[0]['id'],$langid));
 		return $vth->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
-	public function get_label_tip($label,$def = ''){
-		$pdo = $this->pdo;
-		$langid = ($def == '') ? $this->def_lang : $def;
-		$lth = $pdo->prepare("SELECT id FROM lang_label WHERE label= ?");
-		$vth = $pdo->prepare("SELECT tip_value FROM lang_label_data WHERE labelid= ? AND langid= ?");
+	public function get_label_tip($label,$lang = ''){
+		$langid = ($lang == '') ? $this->def_lang : $lang;
+		$lth = $this->pdo->prepare("SELECT id FROM lang_label WHERE label= ?");
+		$vth = $this->pdo->prepare("SELECT tip_value FROM lang_label_data WHERE labelid= ? AND langid= ?");
 		$lth->execute(array($label));
 		$res1 = $lth->fetchAll(PDO::FETCH_ASSOC);
 		$vth->execute(array($res1[0]['id'],$langid));
@@ -69,18 +61,16 @@ class CLanguageLabel
 	}
 
 	public function add_language($id,$name){
-		$pdo = $this->pdo;
-		$check = $pdo->prepare("SELECT * FROM lang WHERE id = ?");
+		$check = $this->pdo->prepare("SELECT * FROM lang WHERE id = ?");
 		$check->execute(array($id));
 		if($check->rowCount() < 1){
-			$ins = $pdo->prepare("INSERT INTO lang (id,name) VALUES(?,?)");
+			$ins = $this->pdo->prepare("INSERT INTO lang (id,name) VALUES(?,?)");
 			return $ins->execute(array($id,$name));
 		}
 		return false;
 	}
 	
 	public function get_languages(){
-		$pdo = $this->pdo;
 		$sql = "SELECT * FROM lang";
 		$stmt = $this->pdo->Query($sql);
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
